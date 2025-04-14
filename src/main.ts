73714f6ae72561d7d8dd99544cda03a43c321b62
若海 <wang@rehiy.com>
2025-04-14 01:37:31 +0000

    fix: 加载bs全功能组件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import '@/css/index.less';
 
 import { createApp } from 'vue';
-import App from './app.vue';
+import App from '@/app.vue';
 
 createApp(App).mount('#app');
